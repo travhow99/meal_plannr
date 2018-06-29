b526a3becaceeeceb071120ae3637f92c282864b
travis <trav.horn@gmail.com>
2018-06-29 17:21:53 -0600

    Finally getting POST to database

--- functions.php
+++ functions.php
@@ -5,16 +5,19 @@ $password = "root";
 $dbName = "meal_plannr";
 
 // Create database connection
-$conn = new mysqli($host, $userName, $password, $dbName);
+$conn = mysqli_connect($host, $userName, $password, $dbName);
 
 // Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+  echo 'Connected successfully.';
 }
 
-echo 'Connected successfully.';
-/*
-echo $_POST['meal_name'];
+
+echo '<br>'.$_POST['meal_name'];
+echo '<br>'.$_POST['url'];
 
       mysqli_query($conn,"SELECT * FROM meals");
 
@@ -22,13 +25,13 @@ echo $_POST['meal_name'];
       $url = $_POST['url'];
 
       //Insert Query of SQL
-      mysqli_query($conn, "INSERT INTO meals(meal_name, url) VALUES ('$meal_name', '$url')");
+      mysqli_query($conn, "INSERT INTO meals(meal_name, meal_url) VALUES ('$meal_name', '$url')");
 
 
 
 
 
-$mysqli_close($conn);
-*/
+mysqli_close($conn);
+
 
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <title>Register / Login</title>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -12,6 +15,10 @@
 
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
   <style>
 
@@ -39,61 +46,59 @@
     <form action="functions.php" method="post">
       <div class="form-group">
         <label for="meal_name">Meal Name:</label>
-        <input type="text" class="form-control" id="meal_name">
+        <input type="text" name="meal_name" class="form-control" id="meal_name">
       </div>
       <div class="form-group">
         <label for="url">URL:</label>
-        <input type="text" class="form-control" id="url">
+        <input type="text" name="url" class="form-control" id="url">
       </div>
       <button type="submit" class="btn btn-default">Submit</button>
     </form>
   </div>
 
   <div class="response_msg"></div>
- </div>
- </div>
- </div>
  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
- <script>
+<!--
+<script>
      $(document).ready(function(){
-     $("#contact-form").on("submit",function(e){
-       e.preventDefault();
-       if($("#contact-form [name='meal_name']").val() === '')
-         {
-           $("#contact-form [name='meal_name']").css("border","1px solid red");
-         }
-       else if ($("#contact-form [name='your_email']").val() === '')
-         {
-           $("#contact-form [name='your_email']").css("border","1px solid red");
-         }
-       else
-       {
-         $("#loading-img").css("display","block");
-         var sendData = $( this ).serialize();
-         $.ajax({
-           type: "POST",
-           url: "get_response.php",
-           data: sendData,
-           success: function(data){
-             $("#loading-img").css("display","none");
-             $(".response_msg").text(data);
-             $(".response_msg").slideDown().fadeOut(3000);
-             $("#contact-form").find("input[type=text], input[type=email], textarea").val("");
+       $("#contact-form").on("submit",function(e){
+         e.preventDefault();
+         if($("#contact-form [name='meal_name']").val() === '')
+           {
+             $("#contact-form [name='meal_name']").css("border","1px solid red");
            }
+         else if ($("#contact-form [name='your_email']").val() === '')
+           {
+             $("#contact-form [name='your_email']").css("border","1px solid red");
+           }
+         else
+         {
+           $("#loading-img").css("display","block");
+           var sendData = $( this ).serialize();
+           $.ajax({
+             type: "POST",
+             url: "get_response.php",
+             data: sendData,
+             success: function(data){
+               $("#loading-img").css("display","none");
+               $(".response_msg").text(data);
+               $(".response_msg").slideDown().fadeOut(3000);
+               $("#contact-form").find("input[type=text], input[type=email], textarea").val("");
+             }
+           });
+         }
+       });
 
-         });
-       }
-     });
-
-     $("#contact-form input").blur(function(){
-     var checkValue = $(this).val();
-     if(checkValue != '')
-     {
-     $(this).css("border","1px solid #eeeeee");
-     }
-     });
+       $("#contact-form input").blur(function(){
+       var checkValue = $(this).val();
+       if(checkValue != '')
+         {
+           $(this).css("border","1px solid #eeeeee");
+         }
+       });
      });
  </script>
+ !-->
 
 </body>
 </html>
